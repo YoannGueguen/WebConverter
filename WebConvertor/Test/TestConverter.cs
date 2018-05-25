cfc8815f3f76cfbf55a372dfd448f17cf78ae588
Yoann <yoann-gueguen@hotmail.fr>
2018-05-25 14:25:28 +0200

    test converter

--- WebConvertor/Test/TestConverter.cs
+++ WebConvertor/Test/TestConverter.cs
@@ -7,5 +7,25 @@ namespace WebConvertor.Test
 {
     public class TestConverter
     {
+        //25/05/2018 Convert km and miles
+
+        public double ConvertMilesToKilometers(double miles)
+        {
+            return miles * 1.609344;
+        }
+
+        public double ConvertKilometersToMiles(double kilometers)
+        {
+            return kilometers * 0.621371192;
+        }
+        public double ConvertKilometersToInch(double kilometers)
+        {
+            return kilometers * 39370.0787401578;
+        }
+
+        public double ConvertInchToKilometers(double inch)
+        {
+            return inch * 0.0000254;
+        }
     }
 }
