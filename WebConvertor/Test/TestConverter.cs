32e87055d332a76bf97655a0e2a387ec121a18e0
Yoann <yoann-gueguen@hotmail.fr>
2018-05-25 17:08:07 +0200

    update css index + html

--- WebConvertor/Test/TestConverter.cs
+++ WebConvertor/Test/TestConverter.cs
@@ -9,21 +9,21 @@ namespace WebConvertor.Test
     {
         //25/05/2018 Convert km and miles
 
-        public double ConvertMilesToKilometers(double miles)
+        public double GivenConvertMilesToKilometers(double miles)
         {
             return miles * 1.609344;
         }
 
-        public double ConvertKilometersToMiles(double kilometers)
+        public double GivenConvertKilometersToMiles(double kilometers)
         {
             return kilometers * 0.621371192;
         }
-        public double ConvertKilometersToInch(double kilometers)
+        public double GivenConvertKilometersToInch(double kilometers)
         {
             return kilometers * 39370.0787401578;
         }
 
-        public double ConvertInchToKilometers(double inch)
+        public double GivenConvertInchToKilometers(double inch)
         {
             return inch * 0.0000254;
         }
