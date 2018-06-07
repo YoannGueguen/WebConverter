849f7d2940eadad19aa8689a27a65dbb9c2556d7
Yoann <yoann-gueguen@hotmail.fr>
2018-06-07 14:57:17 +0200

    add yard & foot

--- WebConvertor/Test/TestConverter.cs
+++ WebConvertor/Test/TestConverter.cs
@@ -17,20 +17,41 @@ namespace WebConvertor.Test
             return miles * 1.609344;
         }
         //ConverterKilometerToMiles_0.621371192 - Cette formule de calcul permet de convertie des kilomètres en miles.
-        public static double ConverterKilometerToMiles(double kilometers)
+        public static double ConverterKilometerToMiles(double kilometer)
         {
-            return kilometers * 0.621371192;
+            return kilometer * 0.621371192;
         }
         //ConverterKilometerToInch_39370.0787401578 - Cette formule de calcul permet de convertir des kilomètres en
         //pouce.
-        public static double ConverterKilometerToInch(double kilometers)
+        public static double ConverterKilometerToInch(double kilometer)
         {
-            return kilometers * 39370.0787401578;
+            return kilometer * 39370.0787401578;
         }
         //ConverterInchToKilometer_0.0000254 - Cette formule de calcul permet de convertir des pouces en kilomètres.
         public static double ConverterInchToKilometer(double inch)
         {
             return inch * 0.0000254;
         }
+        //0,3048
+        public static double ConverterKilometerToFoot(double kilometer)
+        {
+            return kilometer * 0.0000254;
+        }
+        //3280,839895013128
+        public static double ConverterFootToKilometer(double foot)
+        {
+            return foot * 3280.839895013128;
+        }
+        //1093,61329834
+        public static double ConverterKilometerToYard(double kilometer)
+        {
+            return kilometer * 1093.61329834;
+        }
+        //0,0009144
+        public static double ConverterYardToKilometer(double foot)
+        {
+            return foot * 0.0009144;
+        }
     }
+}
 }
